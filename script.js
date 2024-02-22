c9ff8d4acca1b534f46079fcb8237454b11d005c
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-22 14:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function newManipulation(array) {
         return new Promise ((res, rej) => {
             setTimeout(() => {
                 const evenNumber =array.filter((num) => num % 2 == 0 )
-                outputInput.innerText= evenNumber.join(' ')
+                outputInput.innerText= evenNumber.join(', ')
                 res (evenNumber);
 
             }, 1000);
@@ -19,12 +19,14 @@ function newManipulation(array) {
     }).then(evenNumber => {
         return new Promise ((res, rej)=> {
             setTimeout(()=> {
-                const evenMulti = evenNumber.map((multi) => multi *2)
-                outputInput.innerText= evenMulti.join(' ')
+                const evenMulti = evenNumber.map((num) => num * 2)
+                outputInput.innerText= evenMulti.join(', ')
                 res(evenMulti);
 
             }, 2000)
         })
+    }).catch(error => {
+        console.error('Error', error)
     })
 }
 
